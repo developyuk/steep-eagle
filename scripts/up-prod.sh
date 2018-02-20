bc1fd0765e48e5a532338cf4b07a14ea2b74c0ff
Andry Yosua <andry.yosua@pinteraktif.com>
2018-02-20 14:45:37 +0700

    update for production

--- scripts/up-prod.sh
+++ scripts/up-prod.sh
@@ -4,5 +4,5 @@ set -ex
 # docker-compose -f docker-compose-build.yml build web-main && \
 # docker-compose -f docker-compose-build.yml up web-main && \
 
-docker-compose -f docker-compose-build.yml build web api-main db-main db-admin && \
-docker-compose -f docker-compose-build.yml up web api-main db-main db-admin
+sudo docker-compose -f docker-compose-build.yml build web api-main db-main db-admin && \
+sudo docker-compose -f docker-compose-build.yml up web api-main db-main db-admin
