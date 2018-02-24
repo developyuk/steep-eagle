0e942c5a79230144c413f81757f2fad6366a439b
Andry Yosua <andry.yosua@pinteraktif.com>
2018-02-24 12:41:47 +0700

    include postgrest to production

--- scripts/up-prod.sh
+++ scripts/up-prod.sh
@@ -4,5 +4,5 @@ set -ex
 # docker-compose -f docker-compose-build.yml build web-main && \
 # docker-compose -f docker-compose-build.yml up web-main && \
 
-sudo docker-compose -f docker-compose-build.yml build db-main api-main db-admin web && \
-sudo docker-compose -f docker-compose-build.yml up db-main api-main  db-admin web
+sudo docker-compose -f docker-compose-build.yml build db-main db-api api-main db-admin web && \
+sudo docker-compose -f docker-compose-build.yml up db-main db-api api-main db-admin web
