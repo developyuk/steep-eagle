fe75b0ef5fe0b7d8c4dd6a448a132e5cbc9d9289
Andry Yosua <andry.yosua@gmail.com>
2018-05-12 06:49:53 +0700

    fixed production

--- scripts/prod/down.sh
+++ scripts/prod/down.sh
@@ -2,5 +2,4 @@
 set -x
 
 sudo docker-compose -f docker-compose-prod.yml stop
-#sudo docker stop emq220
-#sudo docker rm emq220
+#sudo docker stop emq220 && sudo docker rm emq220
--- scripts/prod/up.sh
+++ scripts/prod/up.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 set -x
 
-#sudo docker stop emq220
-#sudo docker rm emq220
-#sudo docker run -tid --name emq220 -p 1883:1883 -p 8083:8083 -p 8883:8883 -p 8084:8084 -p 18083:18083 emqttd-docker-v2.2.0
+#sudo docker stop emq220 && sudo docker rm emq220
+#sudo docker run -tid --name emq220 \
+#  --volume /etc/letsencrypt:/etc/letsencrypt \
+#  --env EMQ_DASHBOARD__LISTENER__HTTPS=18084 --env EMQ_DASHBOARD__LISTENER__HTTPS__ACCESS__1="allow all" \
+#  --env EMQ_DASHBOARD__LISTENER__HTTPS__ACCEPTORS=2 --env EMQ_DASHBOARD__LISTENER__HTTPS__MAX_CLIENTS=512 \
+#  --env EMQ_DASHBOARD__LISTENER__HTTPS__ACCESS__1="allow all" \
+#  --env EMQ_DASHBOARD__LISTENER__HTTPS__KEYFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/privkey.pem" \
+#  --env EMQ_DASHBOARD__LISTENER__HTTPS__CERTFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/fullchain.pem" \
+#  --env EMQ_LISTENER__WSS__EXTERNAL__KEYFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/privkey.pem" \
+#  --env EMQ_LISTENER__WSS__EXTERNAL__CERTFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/fullchain.pem" \
+#  --env EMQ_LISTENER__SSL__EXTERNAL__KEYFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/privkey.pem" \
+#  --env EMQ_LISTENER__SSL__EXTERNAL__CERTFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/fullchain.pem" \
+#  --publish 1883:1883 --publish 8883:8883 \
+#  --publish 8083:8083 --publish 8084:8084 \
+#  --publish 18083:18083 --publish 18084:18084 \
+#  emqttd-docker-v2.2.0
 
 sudo docker-compose -f docker-compose-prod.yml build
 echo "waiting mqtt server ready to connect"
