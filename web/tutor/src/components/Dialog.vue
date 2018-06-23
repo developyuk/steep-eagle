f564e2cfc4002cfb2309a10bce0d1c445cdc3035
Andry Yosua <andry.yosua@gmail.com>
2018-06-23 10:41:40 +0700

    update login page

---
+++ web/tutor/src/components/Dialog.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  aside#my-mdc-dialog.mdc-dialog(role="alertdialog" aria-labelledby="my-mdc-dialog-label" aria-describedby="my-mdc-dialog-description")
+    .mdc-dialog__surface
+      header.mdc-dialog__header
+        h2#my-mdc-dialog-label.mdc-dialog__header__title
+          slot(name="header")
+      section#my-mdc-dialog-description.mdc-dialog__body
+        slot
+      footer.mdc-dialog__footer
+        slot(name="footer")
+    .mdc-dialog__backdrop
+</template>
+
+<script>
+  import {MDCDialog} from '@material/dialog';
+  import {MDCRipple} from '@material/ripple';
+
+  export default {
+    mounted() {
+      const $dialog = new MDCDialog(this.$el);
+//      new MDCRipple(this.$el.querySelector('.mdc-button'));
+
+      this.$emit('mounted', $dialog);
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- web/tutor/src/views/Sign.vue
+++ web/tutor/src/views/Sign.vue
@@ -1,43 +1,61 @@
 <template lang="pug">
-  template-empty#sign
-    form(@submit.prevent="sign").login
-      img.logo(src="https://images.weserv.nl/?crop=110,95,88,107&url=dl.dropboxusercontent.com/s/psvta5uwq4s0m5y/logo2.jpg")
-      .mdc-text-field
-        input#my-text-field.mdc-text-field__input(v-model.trim="username" name="username" type="text" required)
-        label.mdc-text-field__label(for="my-text-field") Yourname
-        .mdc-line-ripple
-      .errMsg(v-if="errMsg" v-html="errMsg")
-      button(type="submit").mdc-button.mdc-button--raised Login
-    .powered powered by
-      img(src="https://images.weserv.nl/?h=10&url=dl.dropboxusercontent.com/s/htl2v26j5imxgxa/Group.png")
+  transition(enter-active-class="animated fadeIn" leave-class="animated fadeOut")
+    #sign
+      form(@submit.prevent="sign").login
+        img.logo(src="https://images.weserv.nl/?crop=110,95,88,107&url=dl.dropboxusercontent.com/s/psvta5uwq4s0m5y/logo2.jpg")
+        .mdc-text-field
+          input#my-text-field.mdc-text-field__input(v-model.trim="username" name="username" type="text")
+          label.mdc-floating-label(for="my-text-field") Enter your name
+          .mdc-line-ripple
+
+        transition(enter-active-class="animated fadeInUp" leave-class="animated fadeOutDown")
+          .errMsg(v-if="errMsg" v-html="errMsg")
+        button.mdc-fab(aria-label="Login")
+          span.mdc-fab__icon.material-icons arrow_forward
+        .faq
+          .faq__item
+            a(@click="onClickFaq($event,0)") Why no password?
+          .faq__item
+            a(@click="onClickFaq($event,1)") I'm not sure if i registered?
+      .powered powered by
+        img(src="https://images.weserv.nl/?h=10&url=dl.dropboxusercontent.com/s/htl2v26j5imxgxa/Group.png")
+      my-dialog(@mounted="onDialogMounted")
+        span(v-html="dialogText.opts[dialogText.idx]")
+        template(slot="footer")
+          button.mdc-button.mdc-dialog__footer__button.mdc-dialog__footer__button--accept(type="button") Got It!
 </template>
 
 <script>
   import axios from 'axios';
   import {MDCRipple} from '@material/ripple';
   import {MDCTextField} from '@material/textfield';
-  import TemplateEmpty from '@/templates/TemplateEmpty';
+  import {MDCFloatingLabel} from '@material/floating-label';
+  import MyDialog from '@/components/Dialog';
 
   export default {
     components: {
-      'template-empty': TemplateEmpty,
+      MyDialog
     },
     data() {
       return {
         username: '',
-        errMsg: ''
+        errMsg: '',
+        $dialog: null,
+        dialogText: {
+          idx: null,
+          opts: [
+            'We understand that you value security the most and at some point, it might be ackward to use an app without knowing security is protecting us or not, we actually tried our very best to simplify your tasks and remembering password is one of em.',
+            'Please contact administrator for this matters. Don’t forget to add @cc after your name on the login form. Example: kurie@cc']
+        }
       }
-    },
-    mounted() {
-      const $textfield =new MDCTextField(this.$el.querySelector('.mdc-text-field'));
-      const $button = new MDCRipple(this.$el.querySelector('.mdc-button'));
     },
     methods: {
       sign() {
         const url = `${process.env.VUE_APP_API}/sign`;
         const data = {
-          username: this.username.toLowerCase(),
+          username: this.username.toLowerCase().slice(0, -3),
         };
+        this.errMsg = '';
 
         axios.post(url, data)
           .then(response => {
@@ -54,13 +72,29 @@
               console.log(error);
             }
           })
+      },
+      onClickFaq(e, i) {
+        this.dialogText.idx = i;
+        this.$dialog.show()
+      },
+      onDialogMounted(e) {
+        this.$dialog = e;
       }
+    },
+    mounted() {
+      new MDCTextField(this.$el.querySelector('.mdc-text-field'));
+      new MDCFloatingLabel(this.$el.querySelector('.mdc-floating-label'));
+      new MDCRipple(this.$el.querySelector('.mdc-fab'));
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+  @import "../assets/shared";
+  @import "~sass-bem";
+  @import "~@material/fab/mixins";
+
   #sign {
   }
 
@@ -71,9 +105,44 @@
     transform: translate(-50%, -55%);
     max-width: 30rem;
     width: calc(100% - 5rem);
+
+    .mdc-text-field, .mdc-button {
+      width: 100%;
+    }
+    .logo {
+      width: 5rem;
+
+      margin: 0 auto 4rem auto;
+      display: block;
+    }
   }
 
   button {
     margin-top: 2rem;
+    float: right;
+    @include mdc-fab-accessible(map-get($palettes, red));
+  }
+
+  .faq {
+    margin-top: 2rem;
+    @include e(item) {
+      margin: .75rem 0;
+      &, a {
+        color: #9E9E9E;
+        font-size: .75rem;
+      }
+    }
+  }
+
+  .powered {
+    position: absolute;
+    bottom: 1rem;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 12.025rem;
+    font-size: .75rem;
+    img {
+      margin: 0 .5rem;
+    }
   }
 </style>
