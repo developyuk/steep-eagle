0fa32423b94c248b0ce628c61d292852013453f7
Andry Yosua <andry.yosua@gmail.com>
2018-07-10 22:38:23 +0700

    update docker compose

--- web/ops/config/prod.env.js
+++ web/ops/config/prod.env.js
@@ -1,5 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API: '"https://mtutor.codingcamp.id:91"',
+  API: '"https://mtutor.codingcamp.id:90"',
   DBAPI: '"https://mtutor.codingcamp.id:3000"',
+  WS: '"wss://mtutor.codingcamp.id:8084/mqtt"',
 }
