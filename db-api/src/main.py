d6a7a8ef3ecb949f184a8d8680cb5708e0011637
aiosk <andry.yosua@gmail.com>
2018-08-03 04:50:11 +0700

    add swagger

--- db-api/src/main.py
+++ db-api/src/main.py
@@ -1,15 +1,19 @@
+import os
+
 from eve import Eve
 from eve_sqlalchemy import SQL
 from eve_sqlalchemy.validation import ValidatorSQL
-import os
+from eve_swagger import swagger, add_documentation
 
 from tables import Base
 # from links import before_returning_items, before_returning_item
-from mqtt import sessions
+# from mqtt import sessions
 from auth import MyAuth
 from blueprints import auth, schedules, students, tutor_stats
 
-app = Eve(auth=MyAuth, validator=ValidatorSQL, data=SQL, settings=os.path.abspath('settings.py'))
+app = Eve(auth=MyAuth, validator=ValidatorSQL, data=SQL,
+          settings=os.path.abspath('settings.py'))
+app.register_blueprint(swagger)
 app.register_blueprint(auth.blueprint)
 app.register_blueprint(schedules.blueprint)
 app.register_blueprint(students.blueprint)
@@ -24,5 +28,33 @@ db.Model = Base
 # app.on_fetched_item += before_returning_item
 # app.on_inserted += sessions.sessions_tutor_students
 
+add_documentation({
+    'securityDefinitions': {
+        "MyAuth": {"type": "apiKey", "name": "Authorization", "in": "header"}
+    }
+})
+
+for resource, rd in app.config['DOMAIN'].items():
+    if (rd.get('disable_documentation')
+            or resource.endswith('_versions')):
+        continue
+
+    methods = rd['resource_methods']
+    url = '/%s' % rd['url']
+    for method in methods:
+        add_documentation({'paths': {url: {method.lower(): {
+            "security": [{"MyAuth": []}],
+            'produces': ['application/json']
+        }}}})
+
+    methods = rd['item_methods']
+    item_id = '%sId' % rd['item_title'].lower()
+    url = '/%s/{%s}' % (rd['url'], item_id)
+    for method in methods:
+        add_documentation({'paths': {url: {method.lower(): {
+            "security": [{"MyAuth": []}],
+            'produces': ['application/json']
+        }}}})
+
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=os.environ['DEBUG'])
+    app.run(host='0.0.0.0', debug=os.environ['DEBUG'])
