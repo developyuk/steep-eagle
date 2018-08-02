c5e2e53be83fa1856f68532dc094dc7526fa46f4
aiosk <andry.yosua@gmail.com>
2018-08-03 04:48:43 +0700

    update scripts automation

--- scripts/dev/up.sh
+++ scripts/dev/up.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
 
-docker stop emq220 && docker rm emq220
+sh ./scripts/dev/down.sh
 docker run -tid --name emq220 -p 1883:1883 -p 8083:8083 -p 8883:8883 -p 8084:8084 -p 18083:18083 emqttd-docker-v2.2.0
 
 docker-compose build
@@ -9,4 +9,4 @@ echo "waiting mqtt server ready to connect"
 #sleep 1m
 sleep 13
 docker-compose up -d
-docker-compose logs -f
+docker-compose logs --follow --tail 50
