98d5b7af7353ce073a4d67dc37dd80d99d52faf4
aiosk <andry.yosua@gmail.com>
2018-08-03 08:40:54 +0700

    fix trafik

--- scripts/prod/down.sh
+++ scripts/prod/down.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -x
 
-sudo docker-compose -f docker-compose-prod.yml stop
-sudo docker stop emq220 && sudo docker rm emq220
+docker-compose -f docker-compose-prod.yml stop
+# docker stop emq220 && sudo docker rm emq220
--- scripts/prod/up.sh
+++ scripts/prod/up.sh
@@ -2,25 +2,25 @@
 set -x
 
 sh ./scripts/prod/down.sh
-sudo docker run -tid --name emq220 \
- --volume /etc/letsencrypt:/etc/letsencrypt \
- --env EMQ_DASHBOARD__LISTENER__HTTPS=18084 --env EMQ_DASHBOARD__LISTENER__HTTPS__ACCESS__1="allow all" \
- --env EMQ_DASHBOARD__LISTENER__HTTPS__ACCEPTORS=2 --env EMQ_DASHBOARD__LISTENER__HTTPS__MAX_CLIENTS=512 \
- --env EMQ_DASHBOARD__LISTENER__HTTPS__ACCESS__1="allow all" \
- --env EMQ_DASHBOARD__LISTENER__HTTPS__KEYFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/privkey.pem" \
- --env EMQ_DASHBOARD__LISTENER__HTTPS__CERTFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/fullchain.pem" \
- --env EMQ_LISTENER__WSS__EXTERNAL__KEYFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/privkey.pem" \
- --env EMQ_LISTENER__WSS__EXTERNAL__CERTFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/fullchain.pem" \
- --env EMQ_LISTENER__SSL__EXTERNAL__KEYFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/privkey.pem" \
- --env EMQ_LISTENER__SSL__EXTERNAL__CERTFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/fullchain.pem" \
- --publish 1883:1883 --publish 8883:8883 \
- --publish 8083:8083 --publish 8084:8084 \
- --publish 18083:18083 --publish 18084:18084 \
- emqttd-docker-v2.2.0
-echo "waiting mqtt server ready to connect"
-#sleep 1m
-sleep 13
+# docker run -tid --name emq220 \
+#  --volume /etc/letsencrypt:/etc/letsencrypt \
+#  --env EMQ_DASHBOARD__LISTENER__HTTPS=18084 --env EMQ_DASHBOARD__LISTENER__HTTPS__ACCESS__1="allow all" \
+#  --env EMQ_DASHBOARD__LISTENER__HTTPS__ACCEPTORS=2 --env EMQ_DASHBOARD__LISTENER__HTTPS__MAX_CLIENTS=512 \
+#  --env EMQ_DASHBOARD__LISTENER__HTTPS__ACCESS__1="allow all" \
+#  --env EMQ_DASHBOARD__LISTENER__HTTPS__KEYFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/privkey.pem" \
+#  --env EMQ_DASHBOARD__LISTENER__HTTPS__CERTFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/fullchain.pem" \
+#  --env EMQ_LISTENER__WSS__EXTERNAL__KEYFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/privkey.pem" \
+#  --env EMQ_LISTENER__WSS__EXTERNAL__CERTFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/fullchain.pem" \
+#  --env EMQ_LISTENER__SSL__EXTERNAL__KEYFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/privkey.pem" \
+#  --env EMQ_LISTENER__SSL__EXTERNAL__CERTFILE="\/etc\/letsencrypt\/live\/mtutor.codingcamp.id\/fullchain.pem" \
+#  --publish 1883:1883 --publish 8883:8883 \
+#  --publish 8083:8083 --publish 8084:8084 \
+#  --publish 18083:18083 --publish 18084:18084 \
+#  emqttd-docker-v2.2.0
+# echo "waiting mqtt server ready to connect"
+# #sleep 1m
+# sleep 13
 
-sudo docker-compose -f docker-compose-prod.yml build
-sudo docker-compose -f docker-compose-prod.yml up -d --scale db-api=2
-sudo docker-compose -f docker-compose-prod.yml logs --follow --tail 50
+docker-compose -f docker-compose-prod.yml build
+docker-compose -f docker-compose-prod.yml up -d --scale db-api=2 --remove-orphans
+docker-compose -f docker-compose-prod.yml logs --follow --tail 50
