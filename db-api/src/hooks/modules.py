9f1b7b16527153ed86e4ec12392b9fec1cb2158d
aiosk <andry.yosua@gmail.com>
2018-08-04 11:26:54 +0700

    update event hooks module

---
+++ db-api/src/hooks/modules.py
@@ -0,0 +1,41 @@
+import os
+from pprint import pprint
+
+from flask import current_app as app, jsonify
+from werkzeug import secure_filename
+from minio import Minio
+from minio.error import ResponseError
+
+minioClient = Minio('%s:9000' % os.environ['HOST_IP'],
+                    access_key=os.environ['MINIO_ACCESS_KEY'],
+                    secret_key=os.environ['MINIO_SECRET_KEY'],
+                    secure=False)
+
+
+def module_image(image):
+    url = ''
+    try:
+        filename = secure_filename(image.filename)
+        location = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        image.save(location)
+        minioClient.fput_object('modules-img', filename, location, image.content_type)
+        return minioClient.presigned_put_object('modules-img', filename)
+    except Exception as e:
+        return jsonify({"_status": "ERR", "_error": {'message': str(e), "code": 400}}), 400
+    return url.split('?')[0]
+
+
+def before_patch_item(resource_name, updates, original):
+    image = updates.get('fimage')
+    if resource_name == 'modules' and image:
+        url = module_image(image)
+        updates.update({'image': url})
+
+
+def before_post_item(resource_name, items):
+    pprint(resource_name)
+    for i, item in enumerate(items):
+        image = item.get('fimage')
+        if resource_name == 'modules' and image:
+            url = module_image(image)
+            items[i].update({'image': url})
--- db-api/src/main.py
+++ db-api/src/main.py
@@ -7,7 +7,7 @@ from eve_swagger import swagger, add_documentation
 
 from tables import Base
 # from links import before_returning_items, before_returning_item
-from hooks import patch
+from hooks import modules
 # from mqtt import sessions
 from auth import MyAuth
 from blueprints import auth, schedules, students, tutor_stats
@@ -25,7 +25,8 @@ db = app.data.driver
 Base.metadata.bind = db.engine
 db.Model = Base
 
-app.on_update += patch.before_patch_item
+app.on_update += modules.before_patch_item
+app.on_insert += modules.before_post_item
 # app.on_fetched_resource += before_returning_items
 # app.on_fetched_item += before_returning_item
 # app.on_inserted += sessions.sessions_tutor_students
