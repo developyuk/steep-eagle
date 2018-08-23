8ac9e8a3a3cee3f95eb2a0598f43312959405520
aiosk <andry.yosua@gmail.com>
2018-08-24 06:06:21 +0700

    update and fixed PWA

--- web/tutor/src/App.vue
+++ web/tutor/src/App.vue
@@ -2,53 +2,82 @@
   #app.mdc-typography
     router-view
 </template>
-
+<script>
+export default {
+  mounted() {
+    let deferredPrompt;
+    window.addEventListener("beforeinstallprompt", e => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI notify the user they can add to home screen
+      btnAdd.style.display = "block";
+    });
+    // btnAdd.addEventListener("click", e => {
+    //   // hide our user interface that shows our A2HS button
+    //   btnAdd.style.display = "none";
+    //   // Show the prompt
+    //   deferredPrompt.prompt();
+    //   // Wait for the user to respond to the prompt
+    //   deferredPrompt.userChoice.then(choiceResult => {
+    //     if (choiceResult.outcome === "accepted") {
+    //       console.log("User accepted the A2HS prompt");
+    //     } else {
+    //       console.log("User dismissed the A2HS prompt");
+    //     }
+    //     deferredPrompt = null;
+    //   });
+    // });
+  }
+};
+</script>
 <style lang="scss">
-  @import "assets/shared";
-  @import "assets/material-components-web";
-  @import "~foundation-sites/scss/foundation";
+@import "assets/shared";
+@import "assets/material-components-web";
+@import "~foundation-sites/scss/foundation";
 
-  @include foundation-xy-grid-classes;
-  @include foundation-float-classes;
-  @include foundation-flex-classes
-  @include foundation-visibility-classes;
+@include foundation-xy-grid-classes;
+@include foundation-float-classes;
+@include foundation-flex-classes;
+@include foundation-visibility-classes;
 
+html,
+body {
+  font-family: "Roboto", sans-serif;
+}
 
-  html, body {
-    font-family: 'Roboto', sans-serif;
-  }
+#app {
+  background-color: #f9f9f9;
+  /*background-color: #fff;*/
+  max-width: 30rem;
+  padding: 0;
+  height: 100vh;
+  margin: 0 auto;
+}
 
-  #app {
-    background-color: #f9f9f9;
-    /*background-color: #fff;*/
-    max-width: 30rem;
-    padding: 0;
-    height: 100vh;
-    margin: 0 auto;
-  }
+.errMsg {
+  background-color: pink;
+  color: red;
+  padding: 1rem;
+  margin: 1rem 0;
+}
 
-  .errMsg {
-    background-color: pink;
-    color: red;
-    padding: 1rem;
-    margin: 1rem 0;
-  }
+.mdc-list-item {
+  padding: 0.25rem 1rem;
+}
 
-  .mdc-list-item {
-    padding: .25rem 1rem;
-  }
+.mdc-list-group__subheader {
+  background-color: #e0e0e0;
+  margin: 0;
+  padding: 0.75rem 16px;
+}
 
-  .mdc-list-group__subheader {
-    background-color: #E0E0E0;
-    margin: 0;
-    padding: 0.75rem 16px;
-  }
-
-  /*h1.title {*/
-  /*background-color: var(--mdc-theme-primary, #6200ee);*/
-  /*color: #fff;*/
-  /*text-align: center;*/
-  /*padding: 1rem 0;*/
-  /*margin: 0;*/
-  /*}*/
+/*h1.title {*/
+/*background-color: var(--mdc-theme-primary, #6200ee);*/
+/*color: #fff;*/
+/*text-align: center;*/
+/*padding: 1rem 0;*/
+/*margin: 0;*/
+/*}*/
 </style>
--- web/tutor/src/views/Sign.vue
+++ web/tutor/src/views/Sign.vue
@@ -21,7 +21,7 @@
               a(@click="onClickFaq($event,1)") I'm not sure if i registered?
       .cell.shrink
         .powered powered by
-          img(src="https://images.weserv.nl/?h=10&url=dl.dropboxusercontent.com/s/htl2v26j5imxgxa/Group.png")
+          img(alt="codingcamp.id" src="https://images.weserv.nl/?h=10&url=dl.dropboxusercontent.com/s/htl2v26j5imxgxa/Group.png")
       my-dialog(@mounted="onDialogMounted")
         span(v-html="dialogText.opts[dialogText.idx]")
         template(slot="footer")
@@ -29,117 +29,119 @@
 </template>
 
 <script>
-  import axios from 'axios';
-  import {MDCRipple} from '@material/ripple';
-  import {MDCTextField} from '@material/textfield';
-  import {MDCFloatingLabel} from '@material/floating-label';
-  import MyDialog from '@/components/Dialog';
+import axios from "axios";
+import { MDCRipple } from "@material/ripple";
+import { MDCTextField } from "@material/textfield";
+import { MDCFloatingLabel } from "@material/floating-label";
+import MyDialog from "@/components/Dialog";
 
-  export default {
-    components: {
-      MyDialog
-    },
-    data() {
-      return {
-        username: '',
-        errMsg: '',
-        $dialog: null,
-        dialogText: {
-          idx: null,
-          opts: [
-            'We understand that you value security the most and at some point, it might be ackward to use an app without knowing security is protecting us or not, we actually tried our very best to simplify your tasks and remembering password is one of em.',
-            'Please contact administrator for this matters. Don’t forget to add @cc after your name on the login form. Example: kurie@cc']
-        }
+export default {
+  components: {
+    MyDialog
+  },
+  data() {
+    return {
+      username: "",
+      errMsg: "",
+      $dialog: null,
+      dialogText: {
+        idx: null,
+        opts: [
+          "We understand that you value security the most and at some point, it might be ackward to use an app without knowing security is protecting us or not, we actually tried our very best to simplify your tasks and remembering password is one of em.",
+          "Please contact administrator for this matters. Don’t forget to add @cc after your name on the login form. Example: kurie@cc"
+        ]
       }
-    },
-    methods: {
-      sign() {
-        const url = `${process.env.VUE_APP_DBAPI}/sign`;
-        const data = {
-          username: this.username.toLowerCase().slice(0, -3),
-        };
-        this.errMsg = '';
+    };
+  },
+  methods: {
+    sign() {
+      const url = `${process.env.VUE_APP_DBAPI}/sign`;
+      const data = {
+        username: this.username.toLowerCase().slice(0, -3)
+      };
+      this.errMsg = "";
 
-        axios.post(url, data)
-          .then(response => {
-            localStorage.setItem('token', response.data.token);
-            // console.log(this.$router);
-            this.$router.push('/');
-          })
-          .catch(error => {
+      axios
+        .post(url, data)
+        .then(response => {
+          localStorage.setItem("token", response.data.token);
+          // console.log(this.$router);
+          this.$router.push("/");
+        })
+        .catch(error => {
+          console.log(error);
+          const { status, data } = error.response;
+          if (status !== 200) {
+            this.errMsg = `Check your username.`;
+          } else {
             console.log(error);
-            const {status, data} = error.response;
-            if (status !== 200) {
-              this.errMsg = `Check your username.`;
-            } else {
-              console.log(error);
-            }
-          })
-      },
-      onClickFaq(e, i) {
-        this.dialogText.idx = i;
-        this.$dialog.show()
-      },
-      onDialogMounted(e) {
-        this.$dialog = e;
-      }
+          }
+        });
     },
-    mounted() {
-      new MDCTextField(this.$el.querySelector('.mdc-text-field'));
-      new MDCFloatingLabel(this.$el.querySelector('.mdc-floating-label'));
-      new MDCRipple(this.$el.querySelector('.mdc-fab'));
+    onClickFaq(e, i) {
+      this.dialogText.idx = i;
+      this.$dialog.show();
+    },
+    onDialogMounted(e) {
+      this.$dialog = e;
     }
+  },
+  mounted() {
+    new MDCTextField(this.$el.querySelector(".mdc-text-field"));
+    new MDCFloatingLabel(this.$el.querySelector(".mdc-floating-label"));
+    new MDCRipple(this.$el.querySelector(".mdc-fab"));
   }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-  @import "../assets/shared";
-  @import "~sass-bem";
-  @import "~@material/fab/mixins";
-  @import "~foundation-sites/scss/util/util";
+@import "../assets/shared";
+@import "~sass-bem";
+@import "~@material/fab/mixins";
+@import "~foundation-sites/scss/util/util";
 
-  #sign {
-    min-height: 21rem;
+#sign {
+  min-height: 21rem;
+}
+
+form.login {
+  @include absolute-center;
+  max-width: 30rem;
+  width: calc(100% - 5rem);
+
+  .mdc-text-field {
+    width: 100%;
   }
-
-  form.login {
-    @include absolute-center;
-    max-width: 30rem;
-    width: calc(100% - 5rem);
-
-    .mdc-text-field{
-      width: 100%;
-    }
-    .logo {
-
-    }
+  .logo {
   }
+}
 
-  button {
-    margin-top: 2rem;
-    float: right;
-    @include mdc-fab-accessible(map-get($palettes, red));
-  }
+button {
+  margin-top: 2rem;
+  float: right;
+  @include mdc-fab-accessible(map-get($palettes, red));
+}
 
-  .faq {
-    margin-top: 2rem;
-    @include e(item) {
-      margin: .75rem 0;
-      &, a {
-        color: #9E9E9E;
-        font-size: .875rem;
-      }
-    }
-  }
-
-  .powered {
-    height: 1rem;
-    margin: .5rem 0;
-    text-align: center;
-    font-size: .75rem;
-    img {
-      margin: 0 .5rem;
+.faq {
+  margin-top: 2rem;
+  @include e(item) {
+    margin: 0.75rem 0;
+    &,
+    a {
+      color: #9e9e9e;
+      font-size: 0.875rem;
     }
   }
+}
+
+.powered {
+  height: 1rem;
+  margin: 0.5rem 0;
+  text-align: center;
+  font-size: 0.75rem;
+  img {
+    margin: 0 0.5rem;
+  }
+}
 </style>
