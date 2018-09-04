e98ec95efdaf8dc735793ab615e39fcd2f2d7bee
aiosk <andry.yosua@gmail.com>
2018-09-04 07:20:37 +0700

    delete go api

--- api-main/shared/request.go
+++
@@ -1,8 +0,0 @@
-package shared
-
-type (
-  RequestRest struct {
-    Id string `json:"id,omitempty"`
-    Select string `json:"select,omitempty"`
-  }
-)
--- api-main/shared/rest/const.go
+++
@@ -1,8 +0,0 @@
-package rest
-
-import "os"
-
-var (
-  DbApiUrl = "http://" + os.Getenv("HOST_DB_API") + ":3000"
-  //DbApiUrl = "http://varnish-db-api:6081"
-)
