8ff71c5e735ef23cd84024aad2e75c59b6063f28
aiosk <andry.yosua@gmail.com>
2018-10-17 06:21:32 +0700

    fixup! various new, fix, update, remove

--- scripts/prod/up.sh
+++ scripts/prod/up.sh
@@ -7,8 +7,8 @@ cd $MYPWD/web/ops
 npm run build
 cd $MYPWD/web/tutor
 npm run build
-mv $MYPWD/api-v2/app.yaml $MYPWD/api-v2/src/
-mv $MYPWD/api-v2/requirements.txt $MYPWD/api-v2/src/
+mv $MYPWD/api-v2/app.yaml $MYPWD/api-v2/app/
+mv $MYPWD/api-v2/requirements.txt $MYPWD/api-v2/app/
 
 cd $MYPWD
-gcloud app deploy $MYPWD/web/ops/app.yaml $MYPWD/web/tutor/app.yaml $MYPWD/api-v2/src/app.yaml
+gcloud app deploy $MYPWD/web/ops/app.yaml $MYPWD/web/tutor/app.yaml $MYPWD/api-v2/app/app.yaml
